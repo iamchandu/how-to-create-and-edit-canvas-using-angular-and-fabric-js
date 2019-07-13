7335b346e4f8c531029b47e2eaca1022a01c864a
ChandrasekharaPhaniraju Tammana <iamchandu.1992@gmail.com>
2019-07-13 23:17:14 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,108 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { fabric } from 'fabric';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'canvas-edit';
+export class AppComponent implements OnInit {
+
+  private canvas: any;
+  private textString: string;
+  private size: any = {
+      width: 1200,
+      height: 1000
+  };
+  private OutputContent: string;
+
+  ngOnInit() {
+    this.canvas = new fabric.Canvas('canvas', {
+      hoverCursor: 'pointer',
+      selection: true,
+      selectionBorderColor: 'blue'
+    });
+    this.textString = null;
+    this.canvas.setWidth(this.size.width);
+    this.canvas.setHeight(this.size.height);
+    this.OutputContent = null;
+  }
+  
+  addText() {
+    let textString = this.textString;
+    let text = new fabric.IText(textString, {
+      left: 10,
+      top: 10,
+      fontFamily: 'helvetica',
+      angle: 0,
+      fill: '#000000',
+      scaleX: 0.5,
+      scaleY: 0.5,
+      fontWeight: '',
+      hasRotatingPoint: true
+    });
+    this.extend(text, this.randomId());
+    this.canvas.add(text);
+    this.selectItemAfterAdded(text);
+    this.textString = '';
+  }
+
+  extend(obj, id) {
+    obj.toObject = (function (toObject) {
+      return function () {
+        return fabric.util.object.extend(toObject.call(this), {
+          id: id
+        });
+      };
+    })(obj.toObject);
+  }
+
+  randomId() {
+    return Math.floor(Math.random() * 999999) + 1;
+  }
+
+  selectItemAfterAdded(obj) {
+    this.canvas.discardActiveObject().renderAll();
+    this.canvas.setActiveObject(obj);
+  }
+
+  addFigure(figure) {
+    let add: any;
+    switch (figure) {
+      case 'rectangle':
+        add = new fabric.Rect({
+          width: 200, height: 100, left: 10, top: 10, angle: 0,
+          fill: '#3f51b5'
+        });
+        break;
+      case 'square':
+        add = new fabric.Rect({
+          width: 100, height: 100, left: 10, top: 10, angle: 0,
+          fill: '#4caf50'
+        });
+        break;
+      case 'triangle':
+        add = new fabric.Triangle({
+          width: 100, height: 100, left: 10, top: 10, fill: '#2196f3'
+        });
+        break;
+      case 'circle':
+        add = new fabric.Circle({
+          radius: 50, left: 10, top: 10, fill: '#ff5722'
+        });
+        break;
+    }
+    this.extend(add, this.randomId());
+    this.canvas.add(add);
+    this.selectItemAfterAdded(add);
+  }
+
+  ExportToContent(input) {
+    if(input == 'json'){
+      this.OutputContent = JSON.stringify(this.canvas);
+    }else if(input == 'svg'){
+     this.OutputContent = this.canvas.toSVG();
+    }
+  }
+
 }
